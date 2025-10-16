74e5cf8159111b01f7a0618c951a9e28755e9b9f
DDurdey <daniel.durdey@edu.sait.ca>
2025-10-16 13:33:54 -0600

    made tri and square Prism math, all shape math done

--- src/shapes/SquarePrism.java
+++ src/shapes/SquarePrism.java
@@ -1,6 +1,17 @@
 package shapes;
 
-public class SquarePrism
-{
+public class SquarePrism extends Prism {
+	public SquarePrism(double height, double side) {
+		super(height, side);
+	}
+	
+	@Override
+    public double getBaseArea() {
+        return Math.pow(side, 2);
+    }
 
+    @Override
+    public double getVolume() {
+        return getBaseArea() * height;
+    }
 }
--- src/shapes/TriangularPrism.java
+++ src/shapes/TriangularPrism.java
@@ -1,6 +1,17 @@
 package shapes;
 
-public class TriangularPrism
-{
+public class TriangularPrism extends Prism {
+	public TriangularPrism(double height, double side) {
+		super(height, side);
+	}
+	
+	@Override
+    public double getBaseArea() {
+        return (Math.sqrt(3) / 4) * Math.pow(side, 2);
+    }
 
+    @Override
+    public double getVolume() {
+        return getBaseArea() * height;
+    }
 }
